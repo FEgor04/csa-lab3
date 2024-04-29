498f683dd9f944efec2480f71c060ed504a72d58
Egor Fedorov <fegor2004@gmail.com>
2024-04-29 16:16:33 +0300

    feat(translator): add test for first pass of translator

--- src/translator.py
+++ src/translator.py
@@ -2,3 +2,6 @@ from isa import Instruction
 
 def parse_lines(lines: list[str]) -> list[Instruction]:
     return []
+
+def parse_labels(lines: list[str]) -> dict[str, int]:
+    return {}
--- src/translator_test.py
+++ src/translator_test.py
@@ -1,10 +1,24 @@
 from isa import Instruction, Opcode
-from translator import parse_lines
+from translator import parse_lines, parse_labels
 import unittest
 
 class TestTranslator(unittest.TestCase):
+    @unittest.skip("TBD")
     def test_translate_no_arg(self):
         lines = ["HLT"]
         transformed = parse_lines(lines)
         expected = [Instruction(Opcode.HLT, None)]
         self.assertEquals(transformed, expected)
+
+    def test_first_pass(self):
+        lines = [
+            "LABEL1: HLT",
+            "LABEL2: JMP LABEL1",
+        ]
+        expected_labels = {
+            "LABEL1": 0,
+            "LABEL2": 1,
+        }
+        actual = parse_labels(lines)
+        self.assertEquals(actual, expected_labels)
+
