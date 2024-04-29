7de1a9a10b969d9ec319217ae7a790ed42f54f15
Egor Fedorov <fegor2004@gmail.com>
2024-04-29 17:24:21 +0300

    feat: skip empty lines

--- src/translator.py
+++ src/translator.py
@@ -1,17 +1,25 @@
 from isa import Instruction, Opcode
 import re
 
+
 def parse_int_or_none(a: str) -> int | None:
     try:
         return int(a)
     except:
         return None
 
+
+def is_label(s: str) -> bool:
+    return s != "" and parse_int_or_none(s) is None and s[0] != "'" and s[-1] != "'"
+
+
 def parse_lines(lines: list[str]) -> list[Instruction]:
     instructions = []
     labels = parse_labels(lines)
     for i in range(len(lines)):
         line = lines[i]
+        if line == "":
+            continue
         _, opcode, arg = split_instruction(line)
         instructions += [Instruction(Opcode[opcode], None)]
     return instructions
@@ -23,9 +31,10 @@ def parse_labels(lines: list[str]) -> dict[str, int]:
     for i in range(len(lines)):
         line = lines[i]
         label, _, _ = split_instruction(line)
-        if label != "":
+        if is_label(label):
             labels[label] = i
     return labels
+
 
 def split_instruction(line: str) -> tuple[str, str, str]:
     """Парсит инструкцию и трансформирует ее в кортеж вида (метка, опкод, аргумент)"""
@@ -39,4 +48,6 @@ def split_instruction(line: str) -> tuple[str, str, str]:
             return label.split(":")[0], opcode, ""
         opcode, arg = splitted
         return "", opcode, arg
-    return "", splitted[0], ""
+    if len(splitted) == 1:
+        return "", splitted[0], ""
+    return "", "", ""
