98d45c8e1c4ba0c86ac013b137cc3526aadaa5a3
Egor Fedorov <fegor2004@gmail.com>
2024-04-29 16:41:44 +0300

    feat: implement split_instruction function

--- src/translator.py
+++ src/translator.py
@@ -16,5 +16,15 @@ def parse_labels(lines: list[str]) -> dict[str, int]:
 
 def split_instruction(line: str) -> tuple[str, str, str]:
     """Парсит инструкцию и трансформирует ее в кортеж вида (метка, опкод, аргумент)"""
-    return ("", "", "")
+    splitted = line.split()
+    if len(splitted) == 3:
+        label, opcode, arg = splitted
+        return label.split(":")[0], opcode, arg
+    if len(splitted) == 2:
+        if splitted[0].find(":") != -1:
+            label, opcode = splitted
+            return label.split(":")[0], opcode, ""
+        opcode, arg = splitted
+        return "", opcode, arg
+    return "", splitted[0], ""
 
