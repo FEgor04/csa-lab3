adf6f20defcb76d7adc1b670d2937555e297ea33
Egor Fedorov <fegor2004@gmail.com>
2024-06-01 19:28:12 +0300

    style: fix style errors

--- src/machine.py
+++ src/machine.py
@@ -199,7 +199,7 @@ class ControlUnit:
             Opcode.MUL,
             Opcode.DIV,
             Opcode.MOD,
-            Opcode.CMP
+            Opcode.CMP,
         }:
             self.data_path.signal_latch_buffer(
                 RegisterSelector.OPERAND,
